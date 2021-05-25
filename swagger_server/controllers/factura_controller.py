07158bafff811abae1bb49293d24ac3642ddd808
Rubén Gago Navarro <r.gago@alumnos.upm.es>
2021-05-25 16:37:48 +0200

    Added examples

--- swagger_server/controllers/factura_controller.py
+++ swagger_server/controllers/factura_controller.py
@@ -1,11 +1,48 @@
+from datetime import date
 import connexion
 import six
+import datetime
 
 from swagger_server.models.factura import Factura  # noqa: E501
 from swagger_server.models.http_problem import HTTPProblem  # noqa: E501
 from swagger_server.models.inline_response200 import InlineResponse200  # noqa: E501
 from swagger_server import util
+from flask import make_response
 
+example = [
+    {
+        'factura_id': 5623,
+        'user_id': 21,
+        'vehiculo_id': 67,
+        'fecha_emision': datetime.date(2021, 3, 29),
+        'importe': 234.00,
+        'importe_iva': 283.14,
+        'links': [
+            {
+                "href": "api/v1/trabajo/1111",
+                "rel": "trabajo"
+            },
+            {
+                "href": "api/v1/trabajo/1112",
+                "rel": "trabajo"
+            }
+        ]
+    },
+    {
+        'factura_id': 1234,
+        'user_id': 54,
+        'vehiculo_id': 12,
+        'fecha_emision': datetime.date(1996, 9, 26),
+        'importe': 234.00,
+        'importe_iva': 283.14,
+        'links': [
+            {
+                "href": "api/v1/trabajo/1113",
+                "rel": "trabajo"
+            }
+        ]
+    }
+]
 
 def upm_aos_factura_delete(factura_id):  # noqa: E501
     """Elimina una factura.
@@ -17,7 +54,9 @@ def upm_aos_factura_delete(factura_id):  # noqa: E501
 
     :rtype: None
     """
-    return 'do some magic!'
+    return make_response(
+         "{factura_id} successfully deleted".format(factura_id=factura_id), 204
+    )
 
 
 def upm_aos_factura_fget(factura_id):  # noqa: E501
@@ -30,7 +69,9 @@ def upm_aos_factura_fget(factura_id):  # noqa: E501
 
     :rtype: Factura
     """
-    return 'do some magic!'
+    for factura in example:
+        if (factura["factura_id"] == factura_id):
+            return factura
 
 
 def upm_aos_factura_foptions(factura_id):  # noqa: E501
@@ -43,7 +84,11 @@ def upm_aos_factura_foptions(factura_id):  # noqa: E501
 
     :rtype: None
     """
-    return 'do some magic!'
+    for factura in example:
+        if (factura["factura_id"] == factura_id):
+            return make_response(
+                "GET", "DELETE", "PUT", "POST", "OPTIONS", 204
+            )
 
 
 def upm_aos_factura_options():  # noqa: E501
@@ -54,7 +99,9 @@ def upm_aos_factura_options():  # noqa: E501
 
     :rtype: None
     """
-    return 'do some magic!'
+    return make_response(
+                "GET", "OPTIONS", 204
+            )
 
 
 def upm_aos_factura_post(body):  # noqa: E501
@@ -69,7 +116,9 @@ def upm_aos_factura_post(body):  # noqa: E501
     """
     if connexion.request.is_json:
         body = object.from_dict(connexion.request.get_json())  # noqa: E501
-    return 'do some magic!'
+    return make_response(
+            "{body} successfully created".format(body=body), 201
+        )
 
 
 def upm_aos_factura_put(body, if_match, factura_id):  # noqa: E501
@@ -101,7 +150,9 @@ def upm_aos_factura_uget(user_id):  # noqa: E501
 
     :rtype: Factura
     """
-    return 'do some magic!'
+    for factura in example:
+        if (factura["user_id"] == user_id):
+            return factura
 
 
 def upm_aos_factura_uoptions(user_id):  # noqa: E501
@@ -114,7 +165,11 @@ def upm_aos_factura_uoptions(user_id):  # noqa: E501
 
     :rtype: None
     """
-    return 'do some magic!'
+    for factura in example:
+        if (factura["user_id"] == user_id):
+            return make_response(
+                "GET", "OPTIONS", 204
+            )
 
 
 def upm_aos_factura_vget(vehiculo_id):  # noqa: E501
@@ -127,7 +182,9 @@ def upm_aos_factura_vget(vehiculo_id):  # noqa: E501
 
     :rtype: Factura
     """
-    return 'do some magic!'
+    for factura in example:
+        if (factura["vehiculo_id"] == vehiculo_id):
+            return factura
 
 
 def upm_aos_factura_voptions(vehiculo_id):  # noqa: E501
@@ -140,7 +197,11 @@ def upm_aos_factura_voptions(vehiculo_id):  # noqa: E501
 
     :rtype: None
     """
-    return 'do some magic!'
+    for factura in example:
+        if (factura["vehiculo_id"] == vehiculo_id):
+            return make_response(
+                "GET", "OPTIONS", 204
+            )
 
 
 def upm_aos_facturas_get():  # noqa: E501
@@ -151,4 +212,4 @@ def upm_aos_facturas_get():  # noqa: E501
 
     :rtype: InlineResponse200
     """
-    return 'do some magic!'
+    return example
