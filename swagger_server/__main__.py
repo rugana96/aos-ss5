730d93f2308d190e608bf5bd454070e7e35ea528
Rubén Gago Navarro <r.gago@alumnos.upm.es>
2021-06-10 20:40:41 +0200

    Undo changes

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -9,7 +9,7 @@ def main():
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'Facturas AOS'}, pythonic_params=True)
-    app.run(port=8085)
+    app.run(port=8080)
 
 
 if __name__ == '__main__':
